0529077a33c4e52874aaaa5e470642c2c148e75e
m-alsoufi <mo7ammad.alsoufi@gmail.com>
2020-06-18 15:11:13 -0400

    update upload

--- src/main/java/org/wecancodeit/reviews/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/CategoryController.java
@@ -2,7 +2,9 @@ package org.wecancodeit.reviews;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -16,7 +18,7 @@ public class CategoryController {
       this.categoryStorage=categoryStorage;
   }
 
-  @RequestMapping("categories/{categoryName}")
+  @GetMapping("categories/{categoryName}")
     public String showSingleCategory(@PathVariable String categoryName, Model model){
       model.addAttribute("category", categoryStorage.findCategoryByName(categoryName));
       return "category-template";
--- src/main/java/org/wecancodeit/reviews/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewController.java
@@ -2,7 +2,9 @@ package org.wecancodeit.reviews;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -16,11 +18,18 @@ public class ReviewController {
         this.reviewStorage = reviewStorage;
     }
 
-    @RequestMapping("reviews/{reviewName}")
+    @GetMapping("reviews/{reviewName}")
     public String showSingleReview(@PathVariable String reviewName, Model model) {
         model.addAttribute("review", reviewStorage.findReviewByMake(reviewName));
         return "review-template";
     }
 
+    //now we need to add method to review storage class
+    @PostMapping("reviews/add")
+    public String addReview(String make, String model, int year,  String review, String backGroundPic, Category category, Hashtag... hashtags){
+        Review reviewToAdd = new Review(make, model, year, review, "", category, hashtags);
+        reviewStorage.addReview(reviewToAdd);
+        return "redirect:/";
+    }
 }
 
